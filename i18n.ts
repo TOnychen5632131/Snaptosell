499235737b16ac7a9a75e6b38235ccb2fdae16e0
TOnychen5632131 <162278003+TOnychen5632131@users.noreply.github.com>
2025-09-20 08:18:34 -0700

    3

--- i18n.ts
+++ i18n.ts
@@ -1,5 +1,13 @@
 import {getRequestConfig} from 'next-intl/server';
+import {notFound} from 'next/navigation';
 
-export default getRequestConfig(async ({locale}) => ({
-  messages: (await import(`./messages/${locale}.json`)).default
-}));
+const locales = ['en', 'zh'];
+
+export default getRequestConfig(async ({locale = 'en'}) => {
+  if (!locales.includes(locale as any)) notFound();
+
+  return {
+    locale,
+    messages: (await import(`./messages/${locale}.json`)).default
+  };
+});
